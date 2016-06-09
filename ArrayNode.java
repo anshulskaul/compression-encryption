d29973e575e17dcfa1d84e93a182bc16005f09be
anshulskaul <anshulsaraswatkaul@gmail.com>
2016-06-09 17:44:33 +0530

    Create ArrayNode.java

---
+++ ArrayNode.java
@@ -0,0 +1,12 @@
+package compressionproj;
+
+/**
+ *
+ * @author anshulskaul
+ */
+public class ArrayNode {
+public int frequency;
+public char letter;
+public ArrayNode left;
+public ArrayNode right;
+}
